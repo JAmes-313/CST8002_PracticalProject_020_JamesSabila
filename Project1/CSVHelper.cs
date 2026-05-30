96c434b64d9b30fb4562e90e99ee55cb06c14fc4
James Sabila <sabi0030@algonquinlive.com>
2026-05-30 18:58:03 -0400

    Added CSVHelper Nuget

---
+++ Project1/CSVHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.IO;
+using Project1.Model;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project1
+{
+    internal class CSVHelper
+    {
+        private readonly StreamReader reader;
+        private readonly CsvReader csv;
+
+        public CSVHelper()
+        {
+            X500DistinguishedName path = "people.csv";
+            reader = new StreamReader(path);
+            csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<DataModel>().ToList();
+
+            foreach (var record in records)
+            {
+                Console.WriteLine(record);
+            }
+        }
+    }
+}
---
+++ Project1/Model/DataModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.Model
+{
+    internal class DataModel
+    {
+        private string yearData;
+        private string speciesData;
+        private string commonNameData;
+        private string studySiteData;
+        private string associatedCommunityData;
+        private string retinolData;
+
+        public string year
+        {
+            get { return yearData; }
+            set { yearData = value; }
+        }
+
+        public string species
+        {
+            get { return speciesData; }
+            set { speciesData = value; }
+        }
+
+        public string commonName
+        {
+            get { return commonNameData; }
+            set { commonNameData = value; }
+        }
+
+        public string studySite
+        {
+            get { return studySiteData; }
+            set { studySiteData = value; }
+        }
+
+        public string associatedCommunity
+        {
+            get { return associatedCommunityData; }
+            set { associatedCommunityData = value; }
+        }
+
+        public string retinol
+        {
+            get { return retinolData; }
+            set { retinolData = value; }
+        }
+            
+    }
+}
