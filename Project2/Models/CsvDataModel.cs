9e0e5c04795665c96d525b9c9716717ca61aa36f
James Sabila <sabi0030@algonquinlive.com>
2026-06-21 17:47:11 -0400

    Added CSV Model

---
+++ Project2/Models/CsvDataModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Project2.Models
+{
+    public class CsvDataModel
+    {
+        private string yearData;
+        private string speciesData;
+        private string commonNameData;
+        private string studySiteData;
+        private string associatedCommunityData;
+        private string retinolData;
+
+        public string year { get; set; }
+
+        public string species { get; set; }
+
+        public string commonName { get; set; }
+
+        public string studySite { get; set; }
+
+        public string associatedCommunity { get; set; }
+
+        public string retinol { get; set; }
+    }
+}
