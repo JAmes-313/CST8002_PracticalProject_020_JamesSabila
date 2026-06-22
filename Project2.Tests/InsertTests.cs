99fedfae649411d6724b4962cdcc7f09d9a6ffa0
James Sabila <sabi0030@algonquinlive.com>
2026-06-21 22:40:03 -0400

    Add unit testing

---
+++ Project2.Tests/InsertTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project2.Models;
+
+namespace Project2.Tests
+{
+    public class InsertTests
+    {
+        [Fact]
+        public void AddRecord_IncreasesRecordCount()
+        {
+            // Arrange
+            var data = new List<CsvFullModel>();
+
+            var newRecord = new CsvFullModel
+            {
+                Id = 1,
+                Year = "2024",
+                Species = "Test Species",
+                CommonName = "Test Common Name",
+                StudySite = "Test Site",
+                AssociatedCommunity = "Test Community",
+                Retinol = "100"
+            };
+
+            // Act
+            data.Add(newRecord);
+
+            // Assert
+            Assert.Single(data);
+            Assert.Equal("Test Species", data[0].Species);
+        }
+    }
+}
---
+++ Project2.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Project2.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
